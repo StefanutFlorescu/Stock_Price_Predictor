eda96df7b87c2c75e0ce7f83b08b9cf7d4f43111
StefanutFlorescu <stefanut.florescu@gmail.com>
2025-03-22 00:13:24 +0200

    TimeStamp

--- Python_API/Api.py
+++ Python_API/Api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from model import predict_stock_price
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +10,13 @@ def home():
 @app.route("/stock/<string:ticker>")
 def get_stock_price(ticker):
     price = predict_stock_price(ticker)
-    return jsonify({"ticker": ticker, "price": price})
+    current_time = datetime.now().isoformat()  # Generates timestamp in ISO 8601 format
+    
+    return jsonify({
+        "symbol": ticker,
+        "price": price,
+        "date": current_time
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
